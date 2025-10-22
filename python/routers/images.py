0030d50e1cf2f7c1fe5a6c9e5b5a1007845811d5
Victor Osipov <victorosipovprogrammer@gmail.com>
2025-10-22 23:24:39 +0300

    add multi imgs

--- python/routers/images.py
+++ python/routers/images.py
@@ -5,9 +5,125 @@ from fastapi import UploadFile, File
 from fastapi.responses import FileResponse, Response
 from ultralytics import YOLO
 from PIL import Image
+from typing import List
+import aiohttp
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 model = YOLO('yolo11m.pt')
 router = APIRouter(prefix="")
+
+def imgToByteImg(img):
+    im = Image.fromarray(img[..., ::-1])
+    img_byte_arr = io.BytesIO()
+    im.save(img_byte_arr, format='JPEG')
+    return img_byte_arr.getvalue()
+
+
+boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW"
+def create_multidata(field, data, type):
+
+    return f"""{boundary}
+Content-Disposition: form-data; name="{field}"; filename="{field}"
+Content-Type: {type}
+
+{data}
+{boundary}
+"""
+
+class FormData:
+    def __init__(self):
+        self.boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW"
+        self.data = io.BytesIO()
+
+    def append(self, field, file_name, data):
+        self.data.write(f'--{boundary}\r\n'.encode())
+        self.data.write(f'Content-Disposition: form-data; name="{field}"; filename="{file_name}"\r\n'.encode())
+        self.data.write(b'Content-Type: image/jpg\r\n\r\n')
+        self.data.write(data)
+        self.data.write(b'\r\n')
+
+    def getAll(self):
+        self.data.write(f'--{boundary}--\r\n'.encode())
+        return self.data.getvalue()
+    def getMediaType(self):
+        return f"multipart/form-data; boundary={self.boundary}"
+
+
+@router.post("/echo-image")
+async def echo_image(files: List[UploadFile] = File(...)):
+    # form_data = aiohttp.FormData()
+    form_data = FormData()
+    images = []
+
+    for file in files:
+        content = await file.read()
+        image = Image.open(io.BytesIO(content))
+        images.append(image)
+
+    results = model.predict(images, stream=False)
+
+    print(results)
+
+    response_data = []
+
+    for result in results:
+        response_data.append(
+            imgToByteImg(result.plot())
+        )
+        form_data.append('image', 'image.jpeg', imgToByteImg(result.plot()))
+
+    # multipart = MultipartEncoder(
+    #     fields={
+    #         "image": ("image.jpg", response_data[0], "image/jpeg"),
+    #         "image2": ("image.jpg", response_data[0], "image/jpeg"),
+    #     }
+    # )
+    
+
+     # Создаем бинарные данные (например, изображение)
+    # binary_data = response_data[0]
+    
+    # output = io.BytesIO()
+    
+    # # Текстовая часть
+    # output.write(f'--{boundary}\r\n'.encode())
+    # output.write(b'Content-Disposition: form-data; name="description"\r\n\r\n')
+    # output.write(b'This is a JPG image\r\n')
+    
+    # # Бинарная часть
+    # output.write(f'--{boundary}\r\n'.encode())
+    # output.write(b'Content-Disposition: form-data; name="image"; filename="image.jpg"\r\n')
+    # output.write(b'Content-Type: image/jpg\r\n\r\n')
+    # output.write(binary_data)
+    # output.write(b'\r\n')
+
+    # # Бинарная часть 2
+    # output.write(f'--{boundary}\r\n'.encode())
+    # output.write(b'Content-Disposition: form-data; name="image"; filename="image.jpg"\r\n')
+    # output.write(b'Content-Type: image/jpg\r\n\r\n')
+    # output.write(binary_data)
+    # output.write(b'\r\n')
+    
+    # # Закрытие
+    # output.write(f'--{boundary}--\r\n'.encode())
+    
+    # content_bytes = output.getvalue()
+    
+    return Response(
+        content=form_data.getAll(),
+        media_type=form_data.getMediaType(),
+        # headers={"Content-Length": str(len(content_bytes))}
+    )
+    # return Response(
+    #     content=create_multidata('image', response_data[0], "image/jpeg"),
+    #     media_type=f"multipart/form-data; boundary={boundary}"
+    # )
+    # return Response(
+    #     content=response_data[0],
+    #     media_type="image/jpeg"
+    # )
+
+
 
 
 @router.get("/test")
@@ -17,27 +133,4 @@ def get_images():
     return FileResponse(
         path=file_path,
         media_type='image/jpg'
-    )
-
-def imgToByteImg(img):
-    im = Image.fromarray(img[..., ::-1])
-    img_byte_arr = io.BytesIO()
-    im.save(img_byte_arr, format='JPEG')
-    return img_byte_arr.getvalue()
-
-@router.post("/echo-image")
-async def echo_image(file: UploadFile = File(...)):
-    contents = await file.read()
-    
-    image = Image.open(io.BytesIO(contents))
-    results = model.predict(image, stream=True)
-
-    for result in results:
-        img = result.plot()
-
-    img = imgToByteImg(img)
-    
-    return Response(
-        content=img,
-        media_type="image/jpeg"
     )
--- vue/src/app/App.vue
+++ vue/src/app/App.vue
@@ -3,16 +3,18 @@
         <h1>images:</h1>
         <input ref="files_input" type="file" multiple placeholder="Files" name="files" />
         <button type="submit">Submit</button>
-        <img ref="img_el" />
+        <img v-for="img in images" :key="img.id" :src="img.url"/>
     </form>
 </template>
 
 <script setup lang="ts">
-import { useTemplateRef } from "vue";
+import { ref, useTemplateRef } from "vue";
 
 // fetch('')
 
-const img_el = useTemplateRef<HTMLImageElement>("img_el");
+const images = ref<{id: number, url: string}[]>([])
+
+// const img_el = useTemplateRef<HTMLImageElement>("img_el");
 const files_input = useTemplateRef("files_input");
 
 async function sendFiles() {
@@ -21,17 +23,26 @@ async function sendFiles() {
     const files = files_input.value?.files;
 
     if (files) {
-        formData.append("file", files[0]);
-
+        [...files].forEach((file: Blob) => {
+            formData.append("files", file);
+        });
+        
         const response = await fetch("http://127.0.0.1:8000/echo-image", {
             method: "POST",
             body: formData,
         });
-        const blob = await response.blob();
-        const url_data = URL.createObjectURL(blob);
-        img_el.value!.src = url_data;
+        const form_data = await response.formData();
+        const form_data_files = form_data.getAll('image') as File[]
+        
+        // const url_data = URL.createObjectURL(form_data.get('image') as Blob);
+        // img_el.value!.src = url_data;
 
-        console.log(response);
+        images.value = form_data_files.map((file: File, index) => ({
+            id: index,
+            url: URL.createObjectURL(file)
+        }))
+
+        // console.log(response);
     }
 }
 </script>
